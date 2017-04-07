ebcb82f13b39880a3ef8aaa165ccef1aa4e53aea
Jeff Leininger <jeffleus@gmail.com>
2017-04-07 10:31:12 -0700

    refactor to remove hello and switch user pool
    changed ARN for auth to proper user pool
    removed the starter hello function and code

--- handler.js
+++ handler.js
@@ -5,21 +5,6 @@ const AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
 var sns = new AWS.SNS();
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 module.exports.get = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   var response = {
